5311e17ba7f2c6e888ae73aea936595b60d71c41
jjribeiro1 <jvsr.99@hotmail.com>
2022-09-21 21:04:33 -0300

    componente Home

--- my-app/src/pages/Home/index.tsx
+++ my-app/src/pages/Home/index.tsx
@@ -1,7 +1,38 @@
-import "./style";
+import { ReactComponent as Search } from "assets/icons/search.svg";
+import * as S from "./style";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <S.Home>
+      <S.HomeContent>
+        <header>
+          <S.HomeHeaderDetails>
+            <div>
+              <S.HomeHeaderDetailsLogo>Pizza Fresh</S.HomeHeaderDetailsLogo>
+              <S.HomeHeaderDetailsDate>
+                Aqui ficará a data
+              </S.HomeHeaderDetailsDate>
+            </div>
+            <S.HomeHeaderDetailsSearch>
+              <Search />
+              <input type="text" placeholder="Procure pelo sabor" />
+            </S.HomeHeaderDetailsSearch>
+          </S.HomeHeaderDetails>
+        </header>
+        <div>
+          <S.HomeProductTitle>
+            <b>Pizzas</b>
+          </S.HomeProductTitle>
+          <S.HomeProductList>
+            <p>Lista de produtos aqui</p>
+          </S.HomeProductList>
+        </div>
+      </S.HomeContent>
+      <aside>
+        <p>Detalhes dos pedidos aqui</p>
+      </aside>
+    </S.Home>
+  );
 }
 
 export default Home;
