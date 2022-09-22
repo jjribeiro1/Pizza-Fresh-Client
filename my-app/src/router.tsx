cf661c33ba307d6659762d02b062748dbf7ec746
jjribeiro1 <jvsr.99@hotmail.com>
2022-09-22 19:39:19 -0300

    estilização login

--- my-app/src/router.tsx
+++ my-app/src/router.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import { RoutePath } from 'types/routes';
 import Home from "pages/Home/index";
+import Login from 'pages/Login/index';
 
 const Router = () => {
     return (
         <Routes>
+            <Route path={RoutePath.LOGIN} element={<Login/>}/>
             <Route path={RoutePath.HOME} element={<Home />} />
         </Routes>
     );
