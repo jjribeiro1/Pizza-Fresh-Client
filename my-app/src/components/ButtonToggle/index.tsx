7c99da1b1a8d71013e88dd069f2eb47c3797ccf5
jjribeiro1 <jvsr.99@hotmail.com>
2022-09-22 23:01:32 -0300

    estilização ButtonToggle

---
+++ my-app/src/components/ButtonToggle/index.tsx
@@ -0,0 +1,18 @@
+import * as S from "./style";
+import React, { ButtonHTMLAttributes } from "react";
+
+type ButtonType = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonToggleProps = {
+  value: string;
+  active?: boolean;
+} & ButtonType;
+
+function ButtonToggle({ value, active = false, ...props }: ButtonToggleProps) {
+  return (
+    <S.ButtonToggle {...props} active={active}>
+      {value}
+    </S.ButtonToggle>
+  );
+}
+
+export default ButtonToggle;
--- my-app/src/components/OrderDetails/index.tsx
+++ my-app/src/components/OrderDetails/index.tsx
@@ -1,10 +1,15 @@
 import * as S from "./style";
+import ButtonToggle from "components/ButtonToggle";
 
 function OrderDetails() {
   return (
     <S.OrderDetails>
       <S.OrderDetailsTitle>Detalhes do Pedido</S.OrderDetailsTitle>
-      <S.OrderDetailsButtonGroup></S.OrderDetailsButtonGroup>
+      <S.OrderDetailsButtonGroup>
+        <ButtonToggle active={true} value="Comer no Local" />
+        <ButtonToggle active={false} value="P/ Viagem" />
+        <ButtonToggle active={true} value="Delivery" />
+      </S.OrderDetailsButtonGroup>
       <S.OrderDetailsList>
         <p>Itens do pedido</p>
       </S.OrderDetailsList>
