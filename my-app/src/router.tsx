e8cf1fe40eb05a70dfed6e6b2f0ee48b364e346c
jjribeiro1 <jvsr.99@hotmail.com>
2022-09-24 21:23:35 -0300

    estilização page Settings

--- my-app/src/router.tsx
+++ my-app/src/router.tsx
@@ -1,15 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
-import { RoutePath } from 'types/routes';
+import { Routes, Route } from "react-router-dom";
+import { RoutePath } from "types/routes";
 import Home from "pages/Home/index";
-import Login from 'pages/Login/index';
+import Login from "pages/Login/index";
+import Settings from "pages/Settings";
 
 const Router = () => {
-    return (
-        <Routes>
-            <Route path={RoutePath.LOGIN} element={<Login/>}/>
-            <Route path={RoutePath.HOME} element={<Home />} />
-        </Routes>
-    );
-}
+  return (
+    <Routes>
+      <Route path={RoutePath.LOGIN} element={<Login />} />
+      <Route path={RoutePath.HOME} element={<Home />} />
+      <Route path={RoutePath.SETTINGS} element={<Settings />} />
+    </Routes>
+  );
+};
 
 export default Router;
